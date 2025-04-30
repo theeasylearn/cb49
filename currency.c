61ba8984565be3aa9d28ff7357a07ad4913b69d5
theeasylearn <theeasylearn@gmail.com>
2025-04-30 11:09:02 +0530

    operators.

---
+++ currency.c
@@ -0,0 +1,11 @@
+/*
+        write a program to pay given amount to the customer using denomination of indian currency.
+        // 500 200 100 50 20 10 5 2 1 
+        input : 473
+        output 
+        100 X 4 =  400
+        473 - 400
+        50 x 1 = 50
+        73 - 50 = 23
+        20 x 1 = 20
+*/
