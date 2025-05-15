4780f237c8de5290c00bd5fd42a9849980b4bc7e
theeasylearn <theeasylearn@gmail.com>
2025-05-15 11:05:30 +0530

    loops

--- 16_while_3.c
+++ 16_while_3.c
@@ -1,30 +1,49 @@
 /*
-    write a program to findout given number is prime number or not 
+    write a program to findout given number is prime number or not
 
-    
+    steps
+    ----------------------
+    1 create variable number,reminder,divisor
+    2 accept input from user into number variable
+    3 store 2 into divisor
+    4 get reminder by dividing number with divisor
+      reminder(1) = number(13) % divisor(2)
+    5  if reminder is zero then
+        print message number is not prime number
+    6  divisor(3) = divisor + 1
+
+    7   get reminder by dividing number with divisor
+        reminder(1) = number(13) % divisor(3)
+    8 if reminder is zero then
+        print message number is not prime number
+    9   divisor(4) = divisor + 1
+
+    10 get reminder by dividing number with divisor
+        reminder(1) = number(13) % divisor(4)
+    11 if reminder is zero then
+        print message number is not prime number
 */
 #include <stdio.h>
 void main()
 {
-    float amount, rate, interest;
-    int year,count;
+    int number, reminder, divisor;
+    printf("enter number");
+    scanf("%d", &number);
 
-    printf("enter amount");
-    scanf("%f", &amount);
+    divisor = 2;
 
-    printf("enter rate");
-    scanf("%f", &rate);
-
-    printf("enter year");
-    scanf("%d", &year);
-    count=1; 
-
-    while(count<=year) 
+    while(divisor<number)
     {
-        interest = (amount * rate * 1) / 100;
-        amount = amount + interest;
-        count = count + 1;
+        reminder = number % divisor; //3
+        if (reminder == 0)
+        {
+            printf("this is not prime number");
+            break;
+        }
+        divisor = divisor + 1;
     }
-
-    printf("last year interest = %.2f \namount = %.2f", interest, amount);
+    if(divisor == number)
+    {
+        printf("\n it is prime number");
+    }
 }
